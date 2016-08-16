4588f5f1dc0c5534dd90930ea31cfe8b720b9130
jdtzmn <jdtzmn@gmail.com>
2016-08-16 12:38:49 -0400

    added a dynamic livereload.js

---
+++ www/lib/js/livereload.js
@@ -0,0 +1,4 @@
+let livereloadScript = document.createElement('script')
+livereloadScript.setAttribute('src', 'http://' + window.location.hostname + ':35729/livereload.js')
+livereloadScript.setAttribute('charset', 'utf-8')
+document.body.appendChild(livereloadScript)
