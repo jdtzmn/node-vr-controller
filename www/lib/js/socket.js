72f8cf0a579108c99d89d9bb8df0250ee2b04332
jdtzmn <jdtzmn@gmail.com>
2016-08-28 01:41:17 -0400

    added jsmpeg.js support

--- www/lib/js/socket.js
+++ www/lib/js/socket.js
@@ -1,15 +1,4 @@
 var Image, WebSocket
 let client = new WebSocket('ws://' + window.location.host)
-client.onmessage = function onmessage (event) {
-  if (event.data.size === undefined) {
-    let canvas = document.getElementById('video')
-    var context = canvas.getContext('2d')
-    var image = new Image()
-    image.onload = function () {
-      canvas.width = image.width
-      canvas.height = image.height
-      context.drawImage(image, 0, 0, image.width, image.height)
-    }
-    image.src = event.data
-  }
-}
+let canvas = document.getElementById('video')
+let player = new jsmpeg(client, {canvas: canvas})
