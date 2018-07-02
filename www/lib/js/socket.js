67dc12dae7fb5689df97245e3328f10afb43fbf6
jdtzmn <jdtzmn@gmail.com>
2018-07-02 08:45:13 -0400

    Fixed bugs

--- www/lib/js/socket.js
+++ www/lib/js/socket.js
@@ -3,8 +3,5 @@ const url = `ws://${document.location.host}`
 
 window.client = new window.WebSocket(url)
 
-const player = new window.JSMpeg.Player(url, {
-  canvas,
-  chunkSize: 1024 * 1024 * 2,
-  videoBufferSize: 1024 * 1024
-})
+const player = new window.JSMpeg.Player(url, { canvas })
+player.play()
